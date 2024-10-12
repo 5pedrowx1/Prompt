112f6256fff6def5d2f0ba68db97701ab33f110d
PedrogamerY <73367665+PedrogamerY@users.noreply.github.com>
2024-10-13 00:45:40 +0100

    Add files via upload

---
+++ Toast.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Prompt
+{
+    partial class Toast
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Toast));
+            this.picIcon = new System.Windows.Forms.PictureBox();
+            this.lbType = new System.Windows.Forms.Label();
+            this.toastBorder = new System.Windows.Forms.Panel();
+            this.lbMessage = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picIcon
+            // 
+            this.picIcon.Image = global::Prompt.Properties.Resources.success;
+            this.picIcon.Location = new System.Drawing.Point(12, 12);
+            this.picIcon.Name = "picIcon";
+            this.picIcon.Size = new System.Drawing.Size(33, 35);
+            this.picIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picIcon.TabIndex = 0;
+            this.picIcon.TabStop = false;
+            // 
+            // lbType
+            // 
+            this.lbType.AutoSize = true;
+            this.lbType.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbType.Location = new System.Drawing.Point(51, 9);
+            this.lbType.Name = "lbType";
+            this.lbType.Size = new System.Drawing.Size(39, 17);
+            this.lbType.TabIndex = 1;
+            this.lbType.Text = "Type";
+            // 
+            // toastBorder
+            // 
+            this.toastBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.toastBorder.ForeColor = System.Drawing.Color.Black;
+            this.toastBorder.Location = new System.Drawing.Point(-3, -2);
+            this.toastBorder.Name = "toastBorder";
+            this.toastBorder.Size = new System.Drawing.Size(10, 65);
+            this.toastBorder.TabIndex = 3;
+            // 
+            // lbMessage
+            // 
+            this.lbMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lbMessage.Font = new System.Drawing.Font("Yu Gothic", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbMessage.Location = new System.Drawing.Point(55, 31);
+            this.lbMessage.Multiline = true;
+            this.lbMessage.Name = "lbMessage";
+            this.lbMessage.ReadOnly = true;
+            this.lbMessage.Size = new System.Drawing.Size(231, 20);
+            this.lbMessage.TabIndex = 4;
+            // 
+            // Toast
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(298, 59);
+            this.Controls.Add(this.lbMessage);
+            this.Controls.Add(this.toastBorder);
+            this.Controls.Add(this.lbType);
+            this.Controls.Add(this.picIcon);
+            this.Font = new System.Drawing.Font("Yu Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Toast";
+            this.Text = "Toast";
+            this.Load += new System.EventHandler(this.Toast_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picIcon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picIcon;
+        private System.Windows.Forms.Label lbType;
+        private System.Windows.Forms.Panel toastBorder;
+        private System.Windows.Forms.TextBox lbMessage;
+    }
+}
---
+++ Toast.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Prompt
+{
+    public partial class Toast : Form
+    {
+        int toastX, toastY;
+        const int startY = 100;
+        const int fadeDuration = 1000;
+        const int displayDuration = 2000;
+        Timer fadeInTimer;
+        Timer fadeOutTimer;
+
+        public Toast(string type, string message)
+        {
+            InitializeComponent();
+            lbType.Text = type;
+            lbMessage.Text = message;
+
+            switch (type)
+            {
+                case "Success":
+                    toastBorder.BackColor = Color.Green;
+                    picIcon.Image = Properties.Resources.success;
+                    break;
+                case "Error":
+                    toastBorder.BackColor = Color.Red;
+                    picIcon.Image = Properties.Resources.error;
+                    break;
+                case "Info":
+                    toastBorder.BackColor = Color.Blue;
+                    picIcon.Image = Properties.Resources.info;
+                    break;
+                case "Warning":
+                    toastBorder.BackColor = Color.Yellow;
+                    picIcon.Image = Properties.Resources.warning;
+                    break;
+            }
+
+            fadeInTimer = new Timer();
+            fadeInTimer.Interval = fadeDuration / 20;
+            fadeInTimer.Tick += FadeIn;
+
+            fadeOutTimer = new Timer();
+            fadeOutTimer.Interval = fadeDuration / 20;
+            fadeOutTimer.Tick += FadeOut;
+        }
+
+        private void Toast_Load(object sender, EventArgs e)
+        {
+            Position();
+            this.Opacity = 0;
+            fadeInTimer.Start();
+        }
+
+        private void FadeIn(object sender, EventArgs e)
+        {
+            if (this.Opacity < 1)
+            {
+                this.Opacity += 0.05;
+            }
+            else
+            {
+                fadeInTimer.Stop();
+                Timer displayTimer = new Timer();
+                displayTimer.Interval = displayDuration;
+                displayTimer.Tick += (s, args) =>
+                {
+                    displayTimer.Stop();
+                    fadeOutTimer.Start();
+                };
+                displayTimer.Start();
+            }
+        }
+
+        private void FadeOut(object sender, EventArgs e)
+        {
+            if (this.Opacity > 0)
+            {
+                this.Opacity -= 0.05;
+                toastY -= 1;
+                this.Location = new Point(toastX, toastY);
+            }
+            else
+            {
+                fadeOutTimer.Stop();
+                this.Close();
+            }
+        }
+
+        private void Position()
+        {
+            int screenWidth = Screen.PrimaryScreen.WorkingArea.Width;
+            int screenHeight = Screen.PrimaryScreen.WorkingArea.Height;
+
+            toastX = screenWidth - this.Width;
+            toastY = screenHeight - this.Height;
+
+            this.Location = new Point(toastX, toastY);
+        }
+    }
+}
